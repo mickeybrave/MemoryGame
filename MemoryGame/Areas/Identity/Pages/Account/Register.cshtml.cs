b602a7feccbbf2763bb40d08c0be3c5b5bdb39da
michaelb <bravemick@gmail.com>
2018-10-29 14:26:24 +1300

    Commented code removed Missing js files loading removed js code refactored code lines reduced

--- MemoryGame/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MemoryGame/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -100,9 +100,6 @@ namespace MemoryGame.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    //Prevent newly registered users from being automatically logged by commenting this:
-                    //await _signInManager.SignInAsync(user, isPersistent: false);
-                    //return LocalRedirect(returnUrl);
                     return LocalRedirect("/Identity/Account/ConfirmationSent");
                 }
                 foreach (var error in result.Errors)
